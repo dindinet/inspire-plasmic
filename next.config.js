3264777b0518054eb4b29f7f7a72bffa0a446ea7
dindinet <2939526+dindinet@users.noreply.github.com>
2023-12-10 17:42:30 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   // Turn off React StrictMode for now, as react-aria (used by Plasmic)
   // has some troubles with it. See
   // https://github.com/adobe/react-spectrum/labels/strict%20mode
